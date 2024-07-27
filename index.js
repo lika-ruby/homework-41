d1e25bfff451f0e2ff88c673493a290af2fd1ccb
lika-ruby <mary.havrylova.2011@gmail.com>
2024-07-27 09:39:08 +0300

    makes all

---
+++ index.js
@@ -0,0 +1,35 @@
+const temperatureInCelsius = 20;
+const temperatureInFahrenheit = (9 / 5) * temperatureInCelsius + 32;
+console.log(temperatureInFahrenheit);
+
+const numberOfDays = 31;
+const numberOfHours = numberOfDays * 24;
+const numberOfMinutes = numberOfHours * 60;
+console.log(numberOfHours);
+console.log(numberOfMinutes);
+
+const healthPlayer = 83;
+const energyPlayer = 47;
+console.log(healthPlayer - 29);
+console.log(energyPlayer - 12);
+
+const purchaseAmount = 520;
+const discount = 9 / 10;
+console.log(purchaseAmount * discount);
+
+const stringOfFloat = "857.28475947";
+console.log(Number.parseFloat(stringOfFloat));
+
+const stringOfInt = "49610";
+console.log(Number.parseFloat(stringOfInt));
+
+const number = 64;
+console.log(Math.sqrt(number));
+
+let integer = 856;
+let stringOfNumbers = "50384";
+stringOfNumbers = Number.parseInt(stringOfNumbers);
+console.log(integer);
+console.log(stringOfNumbers);
+console.log(integer.toString());
+console.log(stringOfNumbers.toString());
